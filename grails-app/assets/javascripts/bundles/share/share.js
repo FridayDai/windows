714d00e6665e20ad5f49340a2fc6a268c72d8639
sid.feng <sid.feng@xplusz.com>
2014-12-30 14:31:27 +0800

    RATTWO-126 Admin header and navigation

--- grails-app/assets/javascripts/bundles/share/share.js
+++ grails-app/assets/javascripts/bundles/share/share.js
@@ -1,5 +1,5 @@
 //= require ../../libs/jquery/jquery
-//= require ../../libs/jquery-ui/ui/jquery-ui
+//= require ../../libs/jquery-ui/ui/jquery-ui.custom
 //= require ../../libs/jquery-validation/dist/jquery.validate
 //= require ../../libs/underscore/underscore
 //= require ../../partials/base
