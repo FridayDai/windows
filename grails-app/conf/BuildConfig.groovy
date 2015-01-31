c1dd0a9d6cdbb08c2c4fb49b291771c4f5e8efcb
Albert Zhang <albert.zhang@xplusz.com>
2015-01-31 23:14:53 +0800

    Add compass for travis ci

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -66,7 +66,7 @@ grails.project.dependency.resolution = {
     plugins {
         // plugins for the build system only
         build ":tomcat:7.0.55"
-        build ':aws-elastic-beanstalk:0.3'
+        build ':aws-elastic-beanstalk:0.2'
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
