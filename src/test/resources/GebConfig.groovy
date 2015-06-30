3f6865d29900c4746ebf39285d3e0c0e7422d453
Albert Zhang <albert.zhang@xplusz.com>
2015-06-30 17:14:20 +0800

    Change default dimension for window

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -9,6 +9,7 @@ import geb.report.ReportState
 import geb.report.Reporter
 import geb.report.ReportingListener
 import geb.report.ScreenshotReporter
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
@@ -22,13 +23,21 @@ environments {
 	// run via “./gradlew chromeTest”
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
-		driver = { new ChromeDriver() }
+		driver = {
+			def driverInstance = new ChromeDriver()
+			driverInstance.manage().window().setSize(new Dimension(1280, 768))
+			driverInstance
+		}
 	}
 	
 	// run via “./gradlew firefoxTest”
 	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 	firefox {
-		driver = { new FirefoxDriver() }
+		driver = {
+			def driverInstance = new FirefoxDriver()
+			driverInstance.manage().window().setSize(new Dimension(1280, 768))
+			driverInstance
+		}
 	}
 
     phantomJs {
