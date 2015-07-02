de5fd90b810ac41066554146e948a6e068b7fe32
Albert Zhang <albert.zhang@xplusz.com>
2015-07-02 20:09:31 +0800

    Add sleep 2s for archive all mails

--- src/test/groovy/pages/mail/GmailAppPage.groovy
+++ src/test/groovy/pages/mail/GmailAppPage.groovy
@@ -14,9 +14,9 @@ class GmailAppPage extends Page {
         mailTable { mainContent.find('table.zt', 0) }
         mailContent { previewMain.find('table', role: 'presentation', 0) }
         toolBar { $('div.Cq.aqL').findAll { it.'@gh' == 'mtb' } }
-        chooseButton { toolBar.find('div', role: 'button').findAll { it.'@aria-label' == 'Select' } }
+        chooseButton { toolBar.find('div.T-I.J-J5-Ji.T-Pm.T-I-ax7.L3.J-JN-M-I.G-as3', role: 'button').findAll { it.'@aria-label' == 'Select' } }
         chooseAllCheckbox { chooseButton.find('span', role: 'checkbox', 0) }
-        archiveButton { toolBar.find('div', role: 'button').findAll { it.'@aria-label' == 'Archive' } }
-        refreshButton { toolBar.find('div', role: 'button').findAll { it.'@aria-label' == 'Refresh' } }
+        archiveButton { toolBar.find('div.T-I.J-J5-Ji.lR.T-I-ax7.T-I-Js-IF.ar7', role: 'button').findAll { it.'@aria-label' == 'Archive' } }
+        refreshButton { toolBar.find('div.T-I.J-J5-Ji.nu.T-I-ax7.L3', role: 'button').findAll { it.'@aria-label' == 'Refresh' } }
     }
 }
