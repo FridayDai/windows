afa0b0063e2da30bfa3ed5679e4837279ccfd7f2
John Xu <john.xu@xplusz.com>
2015-08-11 15:20:54 +0800

    RH #comment Switch chromeDriver to firefoxDriver

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -27,7 +27,7 @@ reporter = new ScreenshotReporter()
 // Use Firefox as the default driver
 // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
 driver = {
-	def driverInstance = new ChromeDriver()
+	def driverInstance = new FirefoxDriver()
 	driverInstance.manage().window().setSize(new Dimension(1280, 768))
 	driverInstance
 }
