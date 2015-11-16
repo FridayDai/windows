274cc24ecae75eebd7019bb51112d63e3feec72b
thomas cai <thomas.cai@xplusz.com>
2015-11-16 14:53:05 +0800

    #Comment add the ClientPatientSpecRunner

---
+++ src/test/groovy/specs/ClientPatientSpecRunner.groovy
@@ -0,0 +1,35 @@
+package specs
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import specs.client.ClientFunctionalSpec
+import specs.patient.DASHFunctionalSpec
+import specs.patient.FairleyNasalSymptomFunctionalSpec
+import specs.patient.GeneralAfterFunctionalSpec
+import specs.patient.HOOSFunctionalSpec
+import specs.patient.HarrisHipScoreFunctionalSpec
+import specs.patient.KOOSFunctionalSpec
+import specs.patient.NDIFunctionalSpec
+import specs.patient.NRSBACKFunctionalSpec
+import specs.patient.NRSNECKFunctionalSpec
+import specs.patient.ODIFunctionalSpec
+import specs.patient.QuickDASHFunctionalSpec
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses([
+        ClientFunctionalSpec.class,
+//        PatientSpecRunner.class
+        DASHFunctionalSpec.class,
+        FairleyNasalSymptomFunctionalSpec.class,
+        HarrisHipScoreFunctionalSpec.class,
+        HOOSFunctionalSpec.class,
+        KOOSFunctionalSpec.class,
+        NDIFunctionalSpec.class,
+        NRSBACKFunctionalSpec.class,
+        NRSNECKFunctionalSpec.class,
+        ODIFunctionalSpec.class,
+        QuickDASHFunctionalSpec.class,
+        GeneralAfterFunctionalSpec.class,
+])
+class ClientPatientSpecRunner {
+}
